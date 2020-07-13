966c3f6f45ff9dc01071ea3bec2927f5112aeeec
kundanampuneethkrishna <kundanampuneethkrishna@gmail.com>
2020-07-14 00:39:12 +0530

    Updates

---
+++ precache-manifest.e3a16481d1af1dbb1c347a748b84a5db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6a376d73e06a8b6bce6148d0c70dd50",
+    "url": "/kundanampuneethkrishna/my-react-app.git/index.html"
+  },
+  {
+    "revision": "514d924874a25b8623ea",
+    "url": "/kundanampuneethkrishna/my-react-app.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8849d1da4356ab7dbc55",
+    "url": "/kundanampuneethkrishna/my-react-app.git/static/js/2.a1f1f4dc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kundanampuneethkrishna/my-react-app.git/static/js/2.a1f1f4dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "514d924874a25b8623ea",
+    "url": "/kundanampuneethkrishna/my-react-app.git/static/js/main.f1f4916c.chunk.js"
+  },
+  {
+    "revision": "a7e0c26df32b6b0be100",
+    "url": "/kundanampuneethkrishna/my-react-app.git/static/js/runtime-main.77095e5e.js"
+  },
+  {
+    "revision": "ac1ae8c497a46b4263f35bb0f60d8fc0",
+    "url": "/kundanampuneethkrishna/my-react-app.git/static/media/profile.ac1ae8c4.png"
+  }
+]);
