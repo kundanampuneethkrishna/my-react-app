13f474571b240549a6cdfc9be84effd15405fade
kundanampuneethkrishna <kundanampuneethkrishna@gmail.com>
2020-07-14 16:52:07 +0530

    Updates

---
+++ precache-manifest.52df0590a04164b6d46b986e70998337.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fa4565d9c045b00102c0ce1ddbf99bf",
+    "url": "/kundanampuneethkrishna/my-react-app.git/index.html"
+  },
+  {
+    "revision": "fd0a820abaa6162ea1d2",
+    "url": "/kundanampuneethkrishna/my-react-app.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8849d1da4356ab7dbc55",
+    "url": "/kundanampuneethkrishna/my-react-app.git/static/js/2.a1f1f4dc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kundanampuneethkrishna/my-react-app.git/static/js/2.a1f1f4dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd0a820abaa6162ea1d2",
+    "url": "/kundanampuneethkrishna/my-react-app.git/static/js/main.d1579ee6.chunk.js"
+  },
+  {
+    "revision": "a7e0c26df32b6b0be100",
+    "url": "/kundanampuneethkrishna/my-react-app.git/static/js/runtime-main.77095e5e.js"
+  },
+  {
+    "revision": "ac1ae8c497a46b4263f35bb0f60d8fc0",
+    "url": "/kundanampuneethkrishna/my-react-app.git/static/media/profile.ac1ae8c4.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-react-app/precache-manifest.f750aaa5363ee7ead7588ba33b2dbe51.js"
+  "/kundanampuneethkrishna/my-react-app.git/precache-manifest.52df0590a04164b6d46b986e70998337.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-react-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kundanampuneethkrishna/my-react-app.git/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
