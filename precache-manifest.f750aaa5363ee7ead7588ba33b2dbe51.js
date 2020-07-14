09dc0075f270779e73141af1700f49ca142af747
kundanampuneethkrishna <kundanampuneethkrishna@gmail.com>
2020-07-14 16:41:20 +0530

    Updates

---
+++ precache-manifest.f750aaa5363ee7ead7588ba33b2dbe51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5f70d8612e27cff40c59bebf7efe4bf",
+    "url": "/my-react-app/index.html"
+  },
+  {
+    "revision": "80bec7f4381fa3a54e75",
+    "url": "/my-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8849d1da4356ab7dbc55",
+    "url": "/my-react-app/static/js/2.a1f1f4dc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-react-app/static/js/2.a1f1f4dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80bec7f4381fa3a54e75",
+    "url": "/my-react-app/static/js/main.ab1da986.chunk.js"
+  },
+  {
+    "revision": "435064591b6543e8ca17",
+    "url": "/my-react-app/static/js/runtime-main.c0584337.js"
+  },
+  {
+    "revision": "ac1ae8c497a46b4263f35bb0f60d8fc0",
+    "url": "/my-react-app/static/media/profile.ac1ae8c4.png"
+  }
+]);
